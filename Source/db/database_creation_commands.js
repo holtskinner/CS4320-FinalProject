c151caed7c6f7bdec20dbb5118615cdacce0effc
ILLITHID\Justin <jdhcp3@mail.missouri.edu>
2016-10-31 16:13:50 -0500

    added aditional info to the database_creation_commands and reverted its type

--- Source/db/database_creation_commands.js
+++
@@ -1,6 +0,0 @@
-# Use these commands in the mongo shell to initalize database
-
-db.createCollection("Manifests")
-db.createCollection("Fs.files")
-db.createCollection("Fs.chunks")
-db.createCollection("Users")
