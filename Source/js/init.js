d0900c763cab5692feae30022a0e3b5eda4cdb8c
Holt Skinner <HoltWasHere@gmail.com>
2016-10-31 17:06:42 -0500

    Minor UI updates

--- Source/js/init.js
+++ Source/js/init.js
@@ -2,7 +2,6 @@
   $(function(){
 
     $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-
+    $('.modal').modal();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
