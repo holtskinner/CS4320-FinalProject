8dcaf4f604582ee078c95e8a04b942af20b47de0
Holt Skinner <HoltWasHere@gmail.com>
2016-11-06 17:57:03 -0600

    Moved Searchbox to separate file

---
+++ Source/searchBox.php
@@ -0,0 +1,17 @@
+<nav>
+    <div class="nav-wrapper">
+        <form action="#" method="get">
+            <div class="input-field">
+                <input id="search" type="search" required placeholder="Search Manifests">
+                <label for="search" class="active"><i class="fa fa-search" aria-hidden="true"></i></label> <i class="fa fa-times-circle" aria-hidden="true"></i> </div>
+            <input name="searchCategory" type="radio" id="titleSearch">
+            <label class="searchLabel" for="titleSearch">Title</label>
+            <input name="searchCategory" type="radio" id="authorSearch">
+            <label class="searchLabel" for="authorSearch">Author</label>
+            <input name="searchCategory" type="radio" id="dateSearch">
+            <label class="searchLabel" for="dateSearch">Date</label>
+            <input name="searchCategory" type="radio" id="descriptionSearch">
+            <label class="searchLabel" for="descriptionSearch">Description</label>
+        </form>
+    </div>
+</nav>
