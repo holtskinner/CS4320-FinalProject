e877f479a16742e67c2f9d988855e99ab792e8c3
Holt Skinner <HoltWasHere@gmail.com>
2016-11-06 17:36:03 -0600

    Added google sign in function, changed footer color and text. Ran Atom Beautify for formatting

--- Source/js/init.js
+++ Source/js/init.js
@@ -1,7 +1,12 @@
-(function($){
-  $(function(){
+(function($) {
+    $(function() {
 
-    $('.button-collapse').sideNav();
-    $('.modal').modal();
-  }); // end of document ready
+        $('.button-collapse').sideNav();
+        $('.modal').modal();
+
+        function onSignIn(googleUser) {
+            var profile = googleUser.getBasicProfile();
+            var id_token = googleUser.getAuthResponse().id_token; //Send to PHP Backend (This is in place of their user id for authentication)
+        }
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
