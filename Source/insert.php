2dca2b0e8e9592290e56d1d51383c9e848f7affa
Holt Skinner <holtwashere@gmail.com>
2016-11-09 15:18:24 -0600

    Insert files

--- Source/insert.php
+++ Source/insert.php
@@ -1,31 +1,22 @@
 <?php
     // connect to mongodb
-    // database name: test
-    // collection name: Manifests
     $m = new MongoClient();
     echo "Connection to database successfully";
 
+    // select a database and use gridfs
     $db = $m->selectDB('test');
     $collection=$db->selectCollection('Manifests');
     
     function insert_manifest($Manifest) {
 	$collection.insert($Manifest);
-    } 
-    
-    function update_manifest($object_id,$Manifest) {
-	$collection.update($object_id,$Manifest);
 
     } 
-    
-    function remove_manifest($object_id){
-        $collection.remove(array('ObjectId'=>$object_id));
-    }
 
-    // unit tests
     $Manifest = '{"name": "Justin"}';
     insert_manifest($Manifest);
-    $ManifestQuery = array('name' => 'Justin');
-    $cursor = $collection->find($ManifestQuery);
-    var_dump($cursor);
+    
+    function remove_manifest($object_id){
+    $collection.remove(array('ObjectId'=>$object_id));
+}
 ?>
 
