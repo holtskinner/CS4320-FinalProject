57b95b47d8f926c77aaa38e742795e6bf92ea53c
Bradley Rogers <bsr7w2@mail.missouri.edu>
2016-11-10 15:09:57 -0600

    tweaked footer and table header

--- Source/index.php
+++ Source/index.php
@@ -11,6 +11,10 @@
             <div class="container">
                 <div id="table-header">
                     <h2 id="ocdx-title" class="header center teal-text text-lighten-2">Open Community Data Exchange</h2>
+                    <div class="row center">
+                        <h6 class="header col s12 light">The OCDX community supports an open standard for describing data about online communities, as well as the support tools and workflow patterns for researchers and data providers to share their datasets with the wider community. The OCDX community aims to provide the lowest barriers to contributing/sharing data. We welcome your input, suggesting fields information for inclusion in the OCDX specification.
+                        </h6>
+                    </div>
                 </div>
                 <div class="row center">
                   <?php include "searchBox.php" ?>
