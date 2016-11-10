5554b4fe9f9a3139ae0cc41f1da87bdb7837579f
Holt Skinner <HoltWasHere@gmail.com>
2016-11-10 13:55:55 -0600

    Added more fields to upload form

--- Source/upload.php
+++ Source/upload.php
@@ -10,13 +10,34 @@
         <div class="container center-align">
           <h2 class="header center teal-text text-lighten-2">Upload Manifest</h2>
             <form action="#">
-              <div class="file-field input-field">
+              <div class="row file-field input-field">
                 <div class="btn">
                   <span>File</span>
                   <input type="file">
                 </div>
                 <div class="file-path-wrapper">
                   <input class="file-path validate" type="text" required placeholder="Upload File">
+                </div>
+              </div>
+              <div class="row">
+                <div class="input-field">
+                  <input placeholder="Placeholder" type="text" class="validate">
+                  <label for="first_name">Manifest Title</label>
+                </div>
+              </div>
+              <div class="row">
+                <div class="input-field">
+                  <input placeholder="Placeholder" type="text" class="validate">
+                  <label for="first_name">Author Name</label>
+                </div>
+              </div>
+              <div class="row file-field input-field">
+                <div class="btn">
+                  <span>Scripts</span>
+                  <input type="file">
+                </div>
+                <div class="file-path-wrapper">
+                  <input class="file-path validate" type="text" optional placeholder="Upload File">
                 </div>
               </div>
             </form>
