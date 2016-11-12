52aca3550c11b4eae9372dcd09e022264ab32d57
Bradley Rogers <bsr7w2@mail.missouri.edu>
2016-11-12 14:02:22 -0600

    added browse page that displays dummy data from json file

--- Source/nav.php
+++ Source/nav.php
@@ -1,13 +1,13 @@
 <nav class="white" role="navigation">
     <div class="nav-wrapper container"> <a href="index.php" class="brand-logo">OCDX</a>
         <ul class="right hide-on-med-and-down">
-            <li><a href="#">Browse</a></li>
+            <li><a href="browse.php">Browse</a></li>
             <li><a href="upload.php">Upload</a></li>
             <li><a href="profile.php">Profile</a></li>
             <li><a href="#loginModal">Login</a></li>
         </ul>
         <ul id="nav-mobile" class="side-nav">
-            <li><a href="#">Browse</a></li>
+            <li><a href="browse.php">Browse</a></li>
             <li><a href="upload.php">Upload</a></li>
             <li><a href="profile.php">Profile</a></li>
             <li><a href="#loginModal">Login</a></li>
