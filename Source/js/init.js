afc0c34e8e13bc8bd946b32d99f95634ad7f8b0a
Bradley Rogers <bsr7w2@mail.missouri.edu>
2016-11-27 20:58:45 -0600

    edit pulling from sampleJson

--- Source/js/init.js
+++ Source/js/init.js
@@ -35,5 +35,54 @@
         $(this).css('display', 'none');
         $('#edit-button').css('display', 'inherit');
     });
+
+    
+    $.getJSON('sampleManifest.json', function(data) {
+        console.log(data);
+        $('#standardVersion').val(data.manifests.manifest.standardVersions);
+        $('#id').val(data.manifests.manifest.id);
+        $('#creator').val(data.manifests.manifest.creator);
+        $('#dateCreated').val(data.manifests.manifest.dateCreated);
+        $('#comment').val(data.manifests.manifest.comment);
+        $('#title').val(data.manifests.manifest.researchObject.title);
+        $('#abstract').val(data.manifests.manifest.researchObject.abstract);
+        $('#abstract').trigger('autoresize');
+//        var inputHeight = $('#size-tester').outerHeight(true);
+//        var textareaHeight = $('#abstract').outerHeight(true);
+//        if(textareaHeight <= (inputHeight * 2)){
+//            $('#abstract').height(15 + inputHeight);
+//       }
+//        $('#abstract-wrapper').height('100px');
+//        console.log(textareaHeight);
+        $.each(data.manifests.manifest.researchObject.dates, function(key, value){
+            var label;
+            if(value.label == 'start'){label = 'Start';}
+            else if(value.label == 'end'){label = 'End';}
+            else if(value.label == 'retrieved'){label = 'Retrieved';}
+            else if(value.label == 'created'){label = 'Created';}
+            else{label = 'No Assertion';}
+            var date = value.date;
+            $('.date-placeholder').append('<div class="input-field col s6 m3"><input type="text" value="'+ date +'"><label class="active">'+ label +' Date</label></div>')
+//            $('.label-wrapper .date-placeholder').append('<div class="row"><label class="edit-label">Date - '+ label +':</label></div>');
+//            $('.input-wrapper .date-placeholder').append('<div class="row"><span class="edit-span"><input class="edit-input edit-field" value="'+ date +'" readonly/></span></div>')
+        });
+        $('#oversight').val(data.manifests.manifest.privacyEthics.oversight.label);
+        $('#informedConsent').val(data.manifests.manifest.informedConsent);
+        $('#anonymizedData').val(data.manifests.manifest.anonymizedData[0].label);
+        $('#privacyConsiderations').val(data.manifests.manifest.privacyConsiderations);
+        $('#narrative').val(data.manifests.provenance.narrative);
+        $('#narrative').trigger('autoresize');
+        $('#publication').val(data.manifests.publications[0].publication);
+        $('#locationUrl').val(data.manifests.locations[0].location.url);
+        $('#locationComment').val(data.manifests.locations[0].location.comment);
+        Materialize.updateTextFields();
+        
+        
+    });
+    window.onresize = function() {
+        $('#abstract').trigger('autoresize');
+        $('#narrative').trigger('autoresize');
+    }
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
